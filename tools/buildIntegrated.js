cfb7eaa35143d1adc966eaed4eb251b5222635e7
sinlernick <111036791+sinlerdev@users.noreply.github.com>
2023-01-01 21:10:30 +0200

    formatting for builtInte.js

--- tools/buildIntegrated.js
+++ tools/buildIntegrated.js
@@ -1,15 +1,17 @@
-const target = process.argv[2]
-const version = process.argv[3]
-const { exec, execFileSync } = require('child_process');
-const {writeFileSync} = require("fs")
-const { join } = require('path');
-const deletedir = require('rimraf');
+const target = process.argv[2];
+const version = process.argv[3];
+const { exec, execFileSync } = require("child_process");
+const { writeFileSync } = require("fs");
+const { join } = require("path");
+const deletedir = require("rimraf");
 
-const splitTarget = target.split("/")
-const projectName = splitTarget[splitTarget.length - 1]
+const splitTarget = target.split("/");
+const projectName = splitTarget[splitTarget.length - 1];
 
-exec("wally install", {cwd: target}, () => {
-  writeFileSync(join(`${target}/intRelease.project.json`), `{
+exec("wally install", { cwd: target }, () => {
+  writeFileSync(
+    join(`${target}/intRelease.project.json`),
+    `{
     "name": "${projectName}",
     "tree" : {
       "$path" : "Packages",
@@ -17,22 +19,27 @@ exec("wally install", {cwd: target}, () => {
         "$path" : "lib"
       }
     }
-  }`)
+  }`
+  );
 
-  execFileSync('rojo.exe', ["build", `-o ${projectName}-${version}.rbxm`, `${target}/intRelease.project.json`]);
+  execFileSync("rojo.exe", [
+    "build",
+    `-o ${projectName}-${version}.rbxm`,
+    `${target}/intRelease.project.json`,
+  ]);
 
   deletedir(`${target}/Packages`, (err) => {
     if (err) {
-        console.log(err)
-    } else {
-        deletedir(`${target}/intRelease.project.json`, (secArr) => {
-          if (err) {
-            console.log(secArr)
-            return;
-          }
-
-          console.log("Built the release in the root directory and cleaned up")
-        })
+      console.log(err);
+      return
     }
+
+    deletedir(`${target}/intRelease.project.json`, (secArr) => {
+      if (err) {
+        console.log(secArr);
+        return;
+      }
+      console.log("Built the release in the root directory and cleaned up");
+    });
+  });
 });
-})
