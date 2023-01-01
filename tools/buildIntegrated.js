5b9f525f92e4af3e5dffe94fe0dd90be4b70341e
sinlernick <111036791+sinlerdev@users.noreply.github.com>
2023-01-01 21:04:48 +0200

    proper cleanup

--- tools/buildIntegrated.js
+++ tools/buildIntegrated.js
@@ -3,6 +3,7 @@ const version = process.argv[3]
 const { exec, execFileSync } = require('child_process');
 const {writeFileSync} = require("fs")
 const { join } = require('path');
+const deletedir = require('rimraf');
 
 const splitTarget = target.split("/")
 const projectName = splitTarget[splitTarget.length - 1]
@@ -19,6 +20,19 @@ exec("wally install", {cwd: target}, () => {
   }`)
 
   execFileSync('rojo.exe', ["build", `-o ${projectName}-${version}.rbxm`, `${target}/intRelease.project.json`]);
+
+  deletedir(`${target}/Packages`, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        deletedir(`${target}/intRelease.project.json`, (secArr) => {
+          if (err) {
+            console.log(secArr)
+            return;
+          }
+
+          console.log("Built the release in the root directory and cleaned up")
+        })
+    }
+});
 })
-
-
