a92e58c0611571c77be85eba85c46c9e17c67253
sinlernick <sinler.dev.general@gmail.com>
2023-07-20 07:01:23 +0300

    formatting and fixing selene issues.

--- code-runners/LuauBencher/bootstrapper.lua
+++ code-runners/LuauBencher/bootstrapper.lua
@@ -11,7 +11,7 @@ type configuration = {
 
 function bootstrapper.run(options: configuration)
 	local finalString = "\n\nBenchmarks:"
-	for _, directory: Instance in options.directories do
+	for _, directory: any in options.directories do
 		finalString ..= "\n[>] " .. directory.Name
 		finalString ..= stringifyResult(readDir(directory), "    ")
 		task.wait()
